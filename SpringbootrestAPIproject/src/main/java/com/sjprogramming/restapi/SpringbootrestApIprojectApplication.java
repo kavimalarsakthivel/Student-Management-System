c74be08807ab70966f4a3a2059ac76a0797621fc
kavimalarsakthivel <kavimalarsakthivel88@gmail.com>
2025-07-01 20:32:38 +0530

    commit and push it

---
+++ SpringbootrestAPIproject/src/main/java/com/sjprogramming/restapi/SpringbootrestApIprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sjprogramming.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class SpringbootrestApIprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestApIprojectApplication.class, args);
+	}
+
+}
